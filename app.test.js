5fd467f107672b05f4b0336036378c970bd2ef4f
Akhilesh <akhilushsharma11@gmail.com>
2025-09-13 23:01:14 +0530

    modified package.json and added app test for including test stage

--- app.test.js
+++ app.test.js
@@ -2,9 +2,13 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-    it('should return a 200 OK and "Hello World!"', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-        expect(response.body.message).toBe('Hello World!');
+    it('should return a 200 OK and serve the PNG image', async () => {
+        const response = await request(app)
+            .get('/')
+            .expect(200)
+            .expect('Content-Type', 'image/png'); // Verify the response is an image
+
+        // Check that the image data is not empty
+        expect(response.body.length).toBeGreaterThan(0);
     });
 });
